d2cdde3a1a86ab56fde96731beeb990735ccb18b
Julian Budde <buddejul@gmail.com>
2024-10-29 10:26:27 +0100

    Add run stats to script.

--- slurm_lp_relax.sh
+++ slurm_lp_relax.sh
@@ -12,6 +12,19 @@
 # For 10 sims, 1000 boot, 10_000 obs, local takes 6 minutes
 # Hence, 600 minutes, or 10 hours, for 1000 sims remote.
 
+# Run with 21 Cores, num_sims = 1000, num_boot = 1000, num_points = 20,
+# num_obs = [1_000, 10_000]
+# job id: 17692621
+# pytask: 40 tasks
+# -------
+# Nodes: 1
+# Cores per node: 21
+# CPU Utilized: 3-14:02:14
+# CPU Efficiency: 83.88% of 4-06:34:03 core-walltime
+# Job Wall-clock time: 04:53:03 (= pytask time)
+# Memory Utilized: 3.66 GB
+# Memory Efficiency: 89.16% of 4.10 GB
+
 # --------------------------------------------------------------------------------------
 # Start script
 # --------------------------------------------------------------------------------------
